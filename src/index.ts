c273e759019245274dc6f527001843bd69dfa319
Xicheng Guo <xicheng_guo@wistron.com>
2023-11-03 15:40:33 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("index init")
+import axios, { AxiosRequestConfig } from 'axios';
+export class TinyCRUD {
+    constructor(
+        private owner: string, 
+        private repo: string, 
+        private issueNumber: string, 
+        private accessToken: string) {
+
+    }
+
+    async find () {
+        const config: AxiosRequestConfig = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `https://gitee.com/api/v5/repos/${this.owner}/${this.repo}/issues/${this.issueNumber}/comments?page=1&per_page=10&order=desc`,
+            headers: {
+                'Authorization': this.accessToken
+            }
+        }
+
+        return axios.request(config).then(res=>res.data);
+    }
+}
