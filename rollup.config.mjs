1c64f3cddea3f5e148eedcb035c01aad1a4b8886
GuoXiCheng <1377994267@qq.com>
2023-11-09 20:35:36 +0800

    update rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,24 @@
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default {
 	input: 'src/index.ts',
-	output: {
-		dir: 'dist',
-		format: 'cjs'
-	},
-	plugins: [typescript()]
+	output: [
+		{
+			file: 'dist/tiny-crud.cjs.js', // CommonJS 输出文件
+			format: 'cjs',
+		},
+		{
+			file: 'dist/tiny-crud.esm.js', // ES Module 输出文件
+			format: 'esm',
+		},
+	],
+	plugins: [
+		typescript(),
+		nodeResolve(),
+		commonjs(),
+		babel({ babelHelpers: 'bundled' })
+	]
 };
