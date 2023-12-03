5982da14e436721a70b893e496ce51f6a797ee3b
GuoXiCheng <1377994267@qq.com>
2023-12-03 15:09:14 +0800

    update wx-request

--- src/request-lib/interfaces.ts
+++ src/request-lib/interfaces.ts
@@ -7,6 +7,8 @@ export interface WxRequestOptions {
     url: string;
     method: 'GET' | 'POST';
     header?: object;
+    success: (res: {data: string | Object | ArrayBuffer, statusCode: number}) => void;
+    fail: (errMsg: string, errNo: number) => void;
 }
 
 export interface TinyRequest {
