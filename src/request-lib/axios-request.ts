90af90f68f2663b5af6b3f61a49c06d7471c32c0
Xicheng Guo <xicheng_guo@wistron.com>
2023-12-09 11:39:01 +0800

    update

--- src/request-lib/axios-request.ts
+++ src/request-lib/axios-request.ts
@@ -12,12 +12,12 @@ export class AxiosRequest extends TinyRequest {
 
     async get<T>(url: string): Promise<T> {
         return new Promise((resolve, reject) => {
-            this.axios.get(url, {
+            this.axios.get<T>(url, {
                 headers: {
                     'Authorization': `Bearer ${this.accessToken}`
                 }
             }).then((res) => {
-                resolve(res.data as T);
+                resolve(res.data);
             }).catch(error => {
                 reject(error);
             });
