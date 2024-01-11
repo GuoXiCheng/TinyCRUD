414ab8d1bb8523d0ce4895d0d6d340af36de4eef
Xicheng Guo <xicheng_guo@wistron.com>
2024-01-11 14:01:11 +0800

    update

--- src/__tests__/helper/helper.ts
+++ src/__tests__/helper/helper.ts
@@ -6,6 +6,40 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 
+// const mock = new MockAdapter(axios);
+
+// mock.onGet('https://fake-gitee/api/v5/user').reply(200, {
+//     "id": 1000001,
+//     "login": "***",
+//     "name": "***",
+//     "avatar_url": "https://foruda.gitee.com/avatar/***/***.png",
+//     "url": "https://gitee.com/api/v5/users/***",
+//     "html_url": "https://gitee.com/***",
+//     "remark": "",
+//     "followers_url": "https://gitee.com/api/v5/users/***/followers",
+//     "following_url": "https://gitee.com/api/v5/users/***/following_url{/other_user}",
+//     "gists_url": "https://gitee.com/api/v5/users/***/gists{/gist_id}",
+//     "starred_url": "https://gitee.com/api/v5/users/***/starred{/owner}{/repo}",
+//     "subscriptions_url": "https://gitee.com/api/v5/users/***/subscriptions",
+//     "organizations_url": "https://gitee.com/api/v5/users/***/orgs",
+//     "repos_url": "https://gitee.com/api/v5/users/***/repos",
+//     "events_url": "https://gitee.com/api/v5/users/***/events{/privacy}",
+//     "received_events_url": "https://gitee.com/api/v5/users/***/received_events",
+//     "type": "User",
+//     "blog": null,
+//     "weibo": null,
+//     "bio": "",
+//     "public_repos": 0,
+//     "public_gists": 0,
+//     "followers": 0,
+//     "following": 0,
+//     "stared": 0,
+//     "watched": 0,
+//     "created_at": "2020-02-26T13:20:27+08:00",
+//     "updated_at": "2024-01-10T22:27:59+08:00",
+//     "email": null
+// });
+
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
@@ -23,10 +57,14 @@ export const TEST_ENCRYPT_KEY = process.env.TEST_ENCRYPT_KEY as string;
 export const giteeRequest = createRequest({
     httpLib: 'axios',
     httpClient: axios,
+
+    baseURL: undefined,
     accessToken: process.env.TEST_GITEE_TOKEN as string,
+
     platform: 'gitee',
     owner: process.env.TEST_GITEE_OWNER as string,
     repo: process.env.TEST_GITEE_REPO as string,
+
     useEncrypt: true,
     encryptFn: (data: string) => {
         return CryptoJS.AES.encrypt(data, TEST_ENCRYPT_KEY).toString();
